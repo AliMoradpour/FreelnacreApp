f54a40c819256ff65ea292e41ebdb8275a430134
AliMoradpour <mr.mrdp@yahoo.com>
2023-12-30 12:08:02 +0330

    Start project and config it

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="container xl:max-w-screen-xl">
+      <p className="text-3xl font-black">شروع پروژه</p>
+    </main>
+  );
+}
+
+export default App;
