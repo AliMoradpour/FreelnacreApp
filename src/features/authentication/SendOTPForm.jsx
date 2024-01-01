b929472140ffecf590e303e6aecdee6b82f1c490
AliMoradpour <mr.mrdp@yahoo.com>
2024-01-01 19:35:10 +0330

    make the feature structure and add auth page

---
+++ src/features/authentication/SendOTPForm.jsx
@@ -0,0 +1,15 @@
+const SendOTPForm = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <label htmlFor=""></label>
+          <input type="text" />
+        </div>
+        <button>ارسال کد تایید</button>
+      </form>
+    </div>
+  );
+};
+
+export default SendOTPForm;
