b30bbb908f9961a150183605a193fc31ac4fc18c
AliMoradpour <mr.mrdp@yahoo.com>
2024-01-01 22:54:11 +0330

    mutate get OTP

---
+++ src/features/authentication/AuthContainer.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import SendOTPForm from "../authentication/SendOTPForm";
+import CheckOTPForm from "../authentication/CheckOTPForm";
+
+const AuthContainer = () => {
+  const [step, setStep] = useState(1);
+
+  const renderStep = () => {
+    switch (step) {
+      case 1:
+        return <SendOTPForm setStep={setStep}/>;
+      case 2:
+        return <CheckOTPForm />;
+      default:
+        return null;
+    }
+  };
+
+  return <div className="w-full sm:max-w-sm">{renderStep()}</div>;
+};
+
+export default AuthContainer;
--- src/features/authentication/SendOTPForm.jsx
+++ src/features/authentication/SendOTPForm.jsx
@@ -3,8 +3,9 @@ import TextField from "../../ui/TextField";
 import { getOtp } from "../../services/authService";
 import { useMutation } from "@tanstack/react-query";
 import toast from "react-hot-toast";
+import Loading from "../../ui/Loading";
 
-const SendOTPForm = () => {
+const SendOTPForm = ({ setStep }) => {
   const [phoneNumber, setPhoneNumber] = useState("");
 
   const { isPending, error, data, mutateAsync } = useMutation({
@@ -14,10 +15,11 @@ const SendOTPForm = () => {
   const sendOtpHandler = async (e) => {
     e.preventDefault();
     try {
-    const data = await mutateAsync({phoneNumber})
-    toast.success(data.message);
-  } catch (error) {
-    toast.error(error?.response?.data?.message);
+      const data = await mutateAsync({ phoneNumber });
+      setStep(2);
+      toast.success(data.message);
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
     }
   };
 
@@ -30,9 +32,15 @@ const SendOTPForm = () => {
           value={phoneNumber}
           onChange={(e) => setPhoneNumber(e.target.value)}
         />
-        <button type="submit" className="btn btn--primary w-full">
-          ارسال کد تایید
-        </button>
+        <div>
+          {isPending ? (
+            <Loading />
+          ) : (
+            <button type="submit" className="btn btn--primary w-full">
+              ارسال کد تایید
+            </button>
+          )}
+        </div>
       </form>
     </div>
   );
