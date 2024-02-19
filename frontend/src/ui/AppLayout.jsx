22802b7e9661ba0ff505e934c019af43de7372ca
AliMoradpour <mr.mrdp@yahoo.com>
2024-02-19 17:43:19 +0330

    add header and sidebar to a single comp

--- frontend/src/ui/AppLayout.jsx
+++ frontend/src/ui/AppLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 const AppLayout = () => {
   return (
     <div className="grid h-screen grid-rows-[auto_1fr] grid-cols-[15rem_1fr]">
-      <header className="bg-secondary-0 py-4 px-8">App Header</header>
-      <div className="bg-secondary-0 row-start-1 row-span-2">app sidebar</div>
+      <Header />
+    <Sidebar />
       <main className="bg-secondary-100 p-8 overflow-y-auto">
         <div className="mx-auto max-w-screen-md">
           <Outlet />
---
+++ frontend/src/ui/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+  return (
+    <header className="bg-secondary-0 py-4 px-8">App Header</header>
+  )
+}
+
+export default Header
