369c6d4df35f00291dc417524bbf930b2bed27a7
AliMoradpour <mr.mrdp@yahoo.com>
2024-03-24 22:11:51 +0330

    select options , tags and date picker comp

--- frontend/src/features/projects/CreateProjectForm.jsx
+++ frontend/src/features/projects/CreateProjectForm.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import TextField from "../../ui/TextField";
-import { useForm } from "react-hook-form";
+import { set, useForm } from "react-hook-form";
+import RHFSelect from "../../ui/RHFSelect";
+import { TagsInput } from "react-tag-input-component";
+import DatePickerField from "../../ui/DatePickerField";
 
 const CreateProjectForm = () => {
-  const [title, setTitle] = useState("");
+  const [tags, setTags] = useState([]);
+  const [date, setDate] = useState(new Date());
 
   const {
     register,
@@ -31,6 +35,44 @@ const CreateProjectForm = () => {
         }}
         errors={errors}
       />
+      <TextField
+        label="توضیحات"
+        name="title"
+        register={register}
+        required
+        validationSchema={{
+          required: "توضیحات ضروری است",
+          minLength: {
+            value: 15,
+            message: "حداقل 15 کاراکتر وارد کنید",
+          },
+        }}
+        errors={errors}
+      />
+      <TextField
+        label="بودجه"
+        name="price"
+        type="number"
+        register={register}
+        required
+        validationSchema={{
+          required: "بودجه ضروری است",
+        }}
+        errors={errors}
+      />
+
+      <RHFSelect
+        label="دسته بندی"
+        name="category"
+        required
+        register={register}
+        options={[]}
+      />
+      <div>
+        <label className="mb-2 block text-secondary-700">تگ</label>
+        <TagsInput value={tags} onChange={setTags} name="tags" />
+      </div>
+      <DatePickerField label="ددلاین" date={date} setDate={setDate} />
       <button type="submit" className="btn btn--primary w-full">
         تایید
       </button>
