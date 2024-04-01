000161edb93e8ba7324f61f4daa0a42b51a00ea1
AliMoradpour <mr.mrdp@yahoo.com>
2024-04-01 09:24:48 +0330

    Dark Mode on Project

---
+++ frontend/src/context/DarkModeContext.jsx
@@ -0,0 +1,24 @@
+import { createContext, useContext, useState } from "react";
+
+const DarkModeContext = createContext();
+
+export function DarkModeProvider({ children }) {
+  const { isDarkMode, setIsDarkMode } = useState(false);
+  const toggleDarkMode = () => {
+    setIsDarkMode((prev) => !prev);
+  };
+
+  return (
+    <DarkModeContext.Provider value={(isDarkMode, toggleDarkMode)}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
+
+export function useDarkMode() {
+  const context = useContext(DarkModeContext);
+  if (context === undefined)
+    throw new Error("DarkMode Context was Used outside of DarkMode Provider");
+
+  return context;
+}
--- frontend/src/ui/DarkModeToggle.jsx
+++ frontend/src/ui/DarkModeToggle.jsx
@@ -1,9 +1,26 @@
-import { HiOutlineSun } from "react-icons/hi";
+import { HiOutlineSun, HiOutlineMoon } from "react-icons/hi";
+import { useDarkMode } from "../context/DarkModeContext";
 
 function DarkModeToggle() {
+  const { isDarkMode, toggleDarkMode } = useDarkMode();
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark-mode");
+      document.documentElement.classList.remove("light-mode");
+    } else {
+      document.documentElement.classList.add("light-mode");
+      document.documentElement.classList.remove("dark-mode");
+    }
+  }, [isDarkMode]);
+
   return (
-    <button>
-      <HiOutlineSun className="w-5 h-5 text-primary-900" />
+    <button onClik={toggleDarkMode}>
+      {isDarkMode ? (
+        <HiOutlineSun className="w-5 h-5 text-primary-900" />
+      ) : (
+        <HiOutlineMoon className="w-5 h-5 text-primary-900" />
+      )}
     </button>
   );
 }
