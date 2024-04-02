64f08d680a495c17eecdb23d4c38ce23133c4714
AliMoradpour <mr.mrdp@yahoo.com>
2024-04-02 23:18:19 +0330

    freelancer stats

--- frontend/src/services/proposalService.js
+++ frontend/src/services/proposalService.js
@@ -3,3 +3,7 @@ import http from "./httpService";
 export function changeProposalStatusApi({ id, data }) {
   return http.patch(`/proposal/${id}`, data).then(({ data }) => data.data);
 }
+
+export function getProposalApi() {
+  return http.get("/proposals/list").then(({ data }) => data.data);
+}
